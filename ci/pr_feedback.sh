a5b06ec163cc9f11117c06a161933a7fe41b5254
Dávid Csákvári <dodiehun@gmail.com>
2020-06-08 16:21:57 +0200

    Add docs

--- ci/pr_feedback.sh
+++ ci/pr_feedback.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# This script provides build-feedback to a GitHub pull-request based on the parameters.
+# Parameters:
+#   1. GITHUB_PR_STATE: pass "success" to pass the build or "failure" to break it.
+#   2. GITHUB_OWNER: name of the GitHub user or organization where the repository is hosted
+#   3. GITHUB_REPO: name of the GitHub repository
+#   4. GITHUB_COMMIT_ID: commit ID for the build
+#
+# Required environment variable:
+#   GITHUB_TOKEN: access token with 'repo:status' permission. If you define it in a pipeline, make sure that it is encrypted
+#
+# Required environment variables, set up by GoCD:
+#   GO_PIPELINE_NAME
+#   GO_PIPELINE_COUNTER
+#   GO_STAGE_NAME
+#   GO_STAGE_COUNTER
+#   GO_JOB_NAME
+
 GITHUB_PR_STATE="$1"
 GITHUB_OWNER="$2"
 GITHUB_REPO="$3"
